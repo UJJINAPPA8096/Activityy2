5cca060e4b77faa804e5c52424b76c4ffb6bd58d
Ujjinappa pobbula <ujjinappa.p@haviktec.com>
2023-07-03 03:02:20 -0700

    ok

---
+++ Innings.java
@@ -0,0 +1,31 @@
+package com.Assignment2;
+
+public class Innings {
+	private String teamname;
+	private String inningsname;
+	private int runs;
+	
+	public String getTeamname() {
+		return teamname;
+	}
+	public void setTeamname(String teamname) {
+		this.teamname = teamname;
+	}
+	public String getInningsname() {
+		return inningsname;
+	}
+	public void setInningsname(String inningsname) {
+		this.inningsname = inningsname;
+	}
+	public int getRuns() {
+		return runs;
+	}
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+	@Override
+	public String toString() {
+		return "Innings [teamname=" + teamname + ", inningsname=" + inningsname + ", runs=" + runs + "]";
+	}
+	
+}
